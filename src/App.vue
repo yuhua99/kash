b3e345e534e308430c7b81358773ab2d127f64d0
yuhua99 <yuhuahung@synology.com>
2025-07-28 00:58:35 +0800

    feat: remove unnecessary css style

--- src/App.vue
+++ src/App.vue
@@ -13,100 +13,12 @@ const handleLogout = async () => {
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav v-if="authStore.isAuthenticated">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <span class="user-info">Welcome, {{ authStore.user?.username }}!</span>
-        <button @click="handleLogout" class="logout-btn">Logout</button>
-      </nav>
-    </div>
+    <nav v-if="authStore.isAuthenticated">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <button @click="handleLogout">Logout</button>
+    </nav>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-.user-info {
-  display: inline-block;
-  padding: 0 1rem;
-  font-weight: 500;
-}
-
-.logout-btn {
-  display: inline-block;
-  padding: 0 1rem;
-  background: none;
-  border: none;
-  color: inherit;
-  font-size: inherit;
-  cursor: pointer;
-  text-decoration: underline;
-}
-
-.logout-btn:hover {
-  opacity: 0.7;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
