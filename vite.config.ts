838595acc6c2a31e90163e032b8cee3ea99089b5
YOUR_NAME <YOUR_EMAIL>
2025-08-23 18:34:59 +0800

    feat: add vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://budget-api.60009000.xyz',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
