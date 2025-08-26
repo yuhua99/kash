09f02eb8a75a0f433f1d3db42b45b510844cede4
yuhuahung <yuhuahung@synology.com>
2025-08-26 14:12:41 +0800

    chore: exclude shadcn lib when lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -15,7 +15,7 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'src/components/ui/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
