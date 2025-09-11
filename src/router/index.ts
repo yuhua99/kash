487074fa502d6ef87c9f9814a492a0372ad4a174
yuhuahung <yuhuahung@synology.com>
2025-09-11 15:35:41 +0800

    chore: rename

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: DashboardView,
       meta: { requiresAuth: true },
     },
     {
       path: '/transactions',
       name: 'transactions',
-      component: () => import('../views/AllTransactionsView.vue'),
+      component: () => import('../views/TransactionsView.vue'),
       meta: { requiresAuth: true },
     },
     {
