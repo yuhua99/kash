fc54ea7d1f5befb4df97e0b4710336c0fecf6dd3
yuhuahung <yuhuahung@synology.com>
2025-09-23 17:50:00 +0800

    fix: fix layout

--- src/components/ui/table/TableHead.vue
+++ src/components/ui/table/TableHead.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     data-slot="table-head"
     :class="
       cn(
-        'text-muted-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+        'text-muted-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap first:pl-4 last:pr-4 [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
         props.class,
       )
     "
