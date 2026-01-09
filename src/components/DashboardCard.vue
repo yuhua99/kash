4b671a3854f452af4d90dac79d8d7e12f2fda218
yuhuahung <yuhuahung@synology.com>
2026-01-09 15:00:53 +0800

    feat: add reusable DashboardCard component

---
+++ src/components/DashboardCard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="bg-white border border-black h-64 flex flex-col justify-between p-8 transition-colors duration-300"
+  >
+    <slot />
+  </div>
+</template>
